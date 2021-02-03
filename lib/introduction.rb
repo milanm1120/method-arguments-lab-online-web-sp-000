98164b5a2e6d929340a31b03dea6fc884f7026ba
Milan Mody <milanm1120@gmail.com>
2021-02-03 22:51:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,7 @@ end
 introduction("Milan")
 
 def introduction_with_language(name, language)
-  put "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language("Milan", "Ruby")
